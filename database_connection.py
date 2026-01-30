33354c63e96ccc2081f6b1bdcae845aef0130f64
Benjamin <benjaminaravena999@gmail.com>
2026-01-30 19:40:04 -0300

    Fix SQLAlchemy engine poolclass error

--- database_connection.py
+++ database_connection.py
@@ -1,11 +1,13 @@
 from database_data_connection.data_connection import user, password, server, port, database
 from sqlalchemy import create_engine, pool
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 
 #DATABASE_URL = f"postgresql://{user}:{password}@{server}:{port}/{database}"
 #DATABASE_URL = "postgresql://myuser:superpass@dpg-abcd1234-a.oregon-postgres.render.com:5432/mydb"
-DATABASE_URL = "postgresql://myuser:superpass@dpg-abcd1234-a.oregon-postgres.render.com:5432/mydb?sslmode=require"
+#DATABASE_URL = "postgresql://myuser:superpass@dpg-abcd1234-a.oregon-postgres.render.com:5432/mydb?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 

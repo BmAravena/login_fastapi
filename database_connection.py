abc78aa6a09e95a5a6601406afa4f4a9cc841256
Benjamin <benjaminaravena999@gmail.com>
2026-01-31 14:50:53 -0300

    .

--- database_connection.py
+++ database_connection.py
@@ -11,8 +11,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 
-engine = create_engine(DATABASE_URL,
-                       pool_pre_ping=True)
+engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        connect_args={
+            "sslmode": "require"
+        }
+)
 
 sessionLocal = sessionmaker(
     autocommit=False,
